4071994955ac4faab5cb75ec4d0929800cc798b6
hiro <irie.h126@gmail.com>
2021-12-23 23:33:19 +0900

    PDOLのストアクラスを作成

---
+++ src/main/java/com/example/creditcardreader/datasource/PdolValueStore.java
@@ -0,0 +1,12 @@
+package com.example.creditcardreader.datasource;
+
+/**
+ * PDOL構成用のValue値のstore
+ *
+ * 端末側で設定するタグ値とバリュー値のリストを保持
+ */
+public class PdolValueStore {
+    /**
+     * タグ値に対応する値を返却する
+     */
+}
